704e7caa312c6d6f8b5153a6d3c014e48375fde3
Andrew <andrewcooksey42@gmail.com>
2021-03-11 20:23:51 -0600

    Added Google maps to the bottom of homepage. Fixed multiple layout issues.

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 //this function generates Google Maps
 function initMap() {
 	const ahaduCoffee = {
-		lat: 38.9392898, lng: -94.376208
+		lat: 38.93940481483486, lng: -94.37633942221444
 	};
-	const map = new google.maps.Map(document.getElementById("map"), {
-		zoom: 5,
+	const map = new google.maps.Map(document.getElementById("googleMaps"), {
+		zoom: 10,
 		center: ahaduCoffee,
 	});
+	//create the marker
 	const marker = new google.maps.Marker({
 		position: ahaduCoffee,
 		map: map,
