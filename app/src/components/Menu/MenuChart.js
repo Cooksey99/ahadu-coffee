2a6e6548daecd98763f84b7fe46df16d47c9d823
Cooksey99 <andrewcooksey42@gmail.com>
2022-09-05 13:17:11 -0500

    adjusted navbar and footer for responsiveness

--- app/src/components/Menu/MenuChart.js
+++ app/src/components/Menu/MenuChart.js
@@ -4,48 +4,51 @@ export default function MenuChart() {
 
     return (
         <>
-            <section id="menu-chart">
-                <div id="hot-drinks">
-                    <h3>Hot</h3>
-                    {/* only 8 oz */}
-                    <p>Espresso</p>
-                    <p>Macchiato</p>
-                    <p>Ahadu Macchiato</p>
-                    <p>Campana</p>
+            <section id='menu-container'>
 
-                    {/* all sizes */}
-                    <p>Ahadu Special</p>
-                    <p>Americano</p>
-                    <p>Cappuncino</p>
-                    <p>Latte</p>
-                    <p>Mocha</p>
-                    <p>Dirty Chai</p>
-                </div>
+                <div id="menu-chart">
+                    <div id="hot-drinks">
+                        <h2>Hot</h2>
+                        {/* only 8 oz */}
+                        <p>Espresso</p>
+                        <p>Macchiato</p>
+                        <p>Ahadu Macchiato</p>
+                        <p>Campana</p>
 
-                <div id="iced-drinks">
-                    <h3>Cold</h3>
-                    <p>Cold Brew</p>
-                    <p>Iced Latte</p>
-                    <p>Iced Mocha</p>
-                    <p>Iced Dirty Chai</p>
-                </div>
+                        {/* all sizes */}
+                        <p>Ahadu Special</p>
+                        <p>Americano</p>
+                        <p>Cappuncino</p>
+                        <p>Latte</p>
+                        <p>Mocha</p>
+                        <p>Dirty Chai</p>
+                    </div>
 
-                <div id="blended-drinks">
-                    <h3>Blended</h3>
-                    <p>Ahadu Freeze</p>
-                    <p>Frozen Latte</p>
-                    <p>Blender</p>
-                    <p>Fruit Smoothie</p>
-                </div>
+                    <div id="iced-drinks">
+                        <h2>Cold</h2>
+                        <p>Cold Brew</p>
+                        <p>Iced Latte</p>
+                        <p>Iced Mocha</p>
+                        <p>Iced Dirty Chai</p>
+                    </div>
 
-                <div id="add-ins">
-                    <h4>Add-Ins</h4>
-                    <div>
-                        <h5>Milks</h5>
-                        <p>Almond</p>
-                        <p>Oat</p>
-                        <p>Soy</p>
-                        <p>Coconut</p>
+                    <div id="blended-drinks">
+                        <h2>Blended</h2>
+                        <p>Ahadu Freeze</p>
+                        <p>Frozen Latte</p>
+                        <p>Blender</p>
+                        <p>Fruit Smoothie</p>
+                    </div>
+
+                    <div id="add-ins">
+                        <h4>Add-Ins</h4>
+                        <div>
+                            <h5>Milks</h5>
+                            <p>Almond</p>
+                            <p>Oat</p>
+                            <p>Soy</p>
+                            <p>Coconut</p>
+                        </div>
                     </div>
                 </div>
             </section>
--- app/src/components/Navbar/Navbar.js
+++ app/src/components/Navbar/Navbar.js
@@ -7,16 +7,20 @@ export default function Navbar() {
 
     return (
         <>
-            <section id='navbar'>
-                <nav className="links">
-                    <img className="logo" src="https://raw.githubusercontent.com/Cooksey99/ahadu-coffee/master/img/logo.png"
-                        onClick={() => history.push('/')} alt="logo" />
-                    <Link to={'/'}>Home</Link>
-                    <Link to={'/menu'}>Menu</Link>
-                    <Link to={''}>Our Location</Link>
-                    <Link to={''}>Contact Us</Link>
-                </nav>
-            </section>
+            {/* <div> */}
+                <section id='navbar'>
+                    <nav className="links">
+                        <img className="logo" src="https://raw.githubusercontent.com/Cooksey99/ahadu-coffee/master/img/logo.png"
+                            onClick={() => history.push('/')} alt="logo" />
+                        <Link to={'/'}><h2>Home</h2></Link>
+                        <Link to={'/menu'}><h2>Menu</h2></Link>
+                        <Link to={''}><h2>Our Location</h2></Link>
+                        <Link to={''}><h2>Contact Us</h2></Link>
+                        <div className='logo'/>
+                    </nav>
+                </section>
+
+            {/* </div> */}
         </>
     )
 }
