1aa085f65f8a3f318cb20d8f06641fdfb1569388
Andrew Cooksey <andrewcooksey42@gmail.com>
2023-05-06 11:13:50 -0500

    updated font

--- app/src/components/DiscoverOurStory/DiscoverOurStory.js
+++ app/src/components/DiscoverOurStory/DiscoverOurStory.js
@@ -4,12 +4,12 @@ export default function DiscoverOurStory() {
 
     return (
         <>
-            <div id='discover-component'>
+            <div className=''>
                 <img className="background" src="https://raw.githubusercontent.com/Cooksey99/ahadu-coffee/master/img/background-img.jpg" />
                 <section className="discover-our-story">
                     <img src="https://raw.githubusercontent.com/Cooksey99/ahadu-coffee/master/img/wife.jpg" />
                     <div>
-                        <h2>Discover Our Story</h2>
+                        <h2 className='text-3xl'>Discover Our Story</h2>
                         <p>Ahadu coffee was established in 2019 by Teddy and Ana Astatke. After years of living in Lee's Summit, they decided to start a new adventure.<br /><br />Ahadu isn't just a coffee shop - it's a community and a window to the world.</p>
                     </div>
                 </section>
