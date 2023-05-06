3afaf0d11a0f09f093ff61da634ac2fc18fb8a67
Andrew Cooksey <andrewcooksey42@gmail.com>
2023-05-06 12:40:52 -0500

    another improvement to navbar

--- app/src/components/Navbar/Navbar.js
+++ app/src/components/Navbar/Navbar.js
@@ -9,12 +9,12 @@ export default function Navbar() {
 
     const floatNav = () => {
         ref?.current?.classList?.add('rounded-full', 'm-2', 'bg-slate-100')
-        ref?.current?.classList?.remove('bg-slate-300')
+        ref?.current?.classList?.remove('frosted-glass')
 
     }
     const blockNav = () => {
         ref?.current?.classList?.remove('rounded-full', 'm-2', 'bg-slate-100')
-        ref?.current?.classList?.add('bg-slate-300')
+        ref?.current?.classList?.add('frosted-glass')
     }
 
     useEffect = () => {
@@ -43,7 +43,7 @@ export default function Navbar() {
         <>
             {/* <div> */}
             <section className='fixed flex justify-center align-center h-24 w-full z-50'>
-                <nav className="links bg-slate-300 z-50 px-8 py-2 shadow duration-700" ref={ref}>
+                <nav className="links frosted-glass z-50 px-8 py-2 shadow duration-700" ref={ref}>
                     <img className="logo" src="https://raw.githubusercontent.com/Cooksey99/ahadu-coffee/master/img/logo.png"
                         onClick={() => navigate('/')} alt="logo" />
                     <Link to={'/'}><h2>Home</h2></Link>
