7409d69dedfd0d7a7169d6f8b2ff1da893708a2d
Raphael Nembhard <raphaelnembhard@Raphaels-MBP.fios-router.home>
2023-08-17 15:54:49 -0400

    new menu list

--- app/src/components/Menu/MenuChart.js
+++ app/src/components/Menu/MenuChart.js
@@ -5,8 +5,100 @@ export default function MenuChart() {
 
     const [page, setPage] = useState([]);
 
-    const hotList = ['hot1', 'hot2', 'hot3'];
-    const coldList = ['cold1', 'cold2', 'cold3'];
+    const hotList = [
+        "Espresso",
+        "Macchiato",
+        "Ahadu Macchiato",
+        "Campana",
+        "Ahadu Special",
+        "Americano",
+        "Cappucino",
+        "Latte",
+        "Mocha",
+        "Dirty Chai"
+      ];
+      const icedList = [
+        "Cold Brew",
+        "Iced Latte",
+        "Iced Mocha",
+        "Iced Dirty Chai"
+      ];
+    const bakeryList = [
+        "Red Velvet Muffins",
+        "Cranberry Orange Muffins",
+        "Chocolate Chip Muffins",
+        "Healthy Harvest Muffins",
+        "Blueberry Muffins",
+        "Banana Nut Muffins",
+        "Gooey Butter Cookies",
+        "Plain Bagels",
+        "Everything Bagels",
+        "Blueberry Bagels",
+        "Cinnamon Raisin Bagels",
+        "Chocolate Chip Supreme Cookie",
+        "Chocolate Chip Coffee Cake"
+      ];
+      const blendedList = [
+        "Ahadu Freeze",
+        "Frozen Latte",
+        "Blender",
+        "Fruit Smoothie"
+      ];
+      const coffeeList = [
+        "Drip",
+        "Cafe Au Lait",
+        "Shot In The Dark",
+        "Pour Over",
+        "Chemex",
+        "French Press"
+      ];
+      const nonCoffeeList = [
+        "Homestyle Tea",
+        "Italian Soda",
+        "Chocolate Milk",
+        "Iced/Hot Tea",
+        "London Fog",
+        "Chai Tea Latte (Hot or Iced)",
+        "Hot Chocolate"
+      ];
+      const smoothiesList = [
+        "Strawberry",
+        "Strawberry Banana",
+        "Peach",
+        "Raspberry",
+        "Blueberry Pomegranate",
+        "Wild Berry",
+        "Mango"
+      ];
+      const teaList = [
+        "Black",
+        "Green",
+        "Earl Grey",
+        "Lemongrass",
+        "Ginger Peach",
+        "Mango Ceylon",
+        "Ginger Peach Decaf",
+        "Mint Decaf",
+        "Cranberry Blood Orange",
+        "Passion Fruit",
+        "Vanilla Almond",
+        "Caramel Apple Decaf",
+        "House Chai",
+        "More Decaf Options..."
+      ];
+      const bottledList = [
+        "Smart Water",
+        "Jarritos (Mandarin, Guava, Pineapple, Lime)",
+        "Coca-Cola",
+        "Vitamin Water (XXX)",
+        "Vitamin Water Zero (Squeezed Lemonade)"
+      ];
+      const addOnsList = [
+        "Milk - Almond, Oat, Skim, Soy Coconut",
+        "Sauce - Chocolate, Sugar Free Chocolate, White Chocolate, Caramel",
+        "Syrup - Vanilla, Caramel, Almond, Coconut, Hazelnut, Irish Cream, Almond Roca, Brown Sugar & Cinnamon, Amaretto, Cherry, Tangerine, Blackberry, Raspberry, Sugar Free Vanilla, Strawberry, Peach, Sugar Free Caramel, Sugar Free Chocolate",
+        "Extra Espresso (2 Shots)"
+        ];
 
     const calculatePage = () => {
         return page.map((item, index) => (
@@ -27,15 +119,23 @@ export default function MenuChart() {
                         <button className="cust-button"
                             onClick={() => setPage(hotList)}>Hot</button>
                         <button className="cust-button"
-                            onClick={() => setPage(coldList)}>Cold</button>
-                        <button className="cust-button">Blended</button>
-                        <button className="cust-button">Coffee</button>
-                        <button className="cust-button">Non-Coffee</button>
-                        <button className="cust-button">Smoothies</button>
-                        <button className="cust-button">Tea</button>
-                        <button className="cust-button">Bottled</button>
-                        <button className="cust-button">Add-ons</button>
-                        <button className="cust-button">Bakery</button>
+                            onClick={() => setPage(icedList)}>Cold</button>
+                        <button className="cust-button"
+                            onClick={() => setPage(blendedList)}>Blended</button>
+                        <button className="cust-button"
+                            onClick={() => setPage(coffeeList)}>Coffee</button>
+                        <button className="cust-button"
+                            onClick={() => setPage(nonCoffeeList)}>Non-Coffee</button>
+                        <button className="cust-button"
+                            onClick={() => setPage(smoothiesList)}>Smoothies</button>
+                        <button className="cust-button"
+                            onClick={() => setPage(teaList)}>Tea</button>
+                        <button className="cust-button"
+                            onClick={() => setPage(bottledList)}>Bottled</button>
+                        <button className="cust-button"
+                            onClick={() => setPage(addOnsList)}>Add-ons</button>
+                        <button className="cust-button"
+                            onClick={() => setPage(bakeryList)}>Bakery</button>
                     </div>
                     <div className="bg-slate-300 w-full h-full">
                         <h1>LIST</h1>
